08a491531a998155985fec796d0a855d80dd3307
bansheerubber <gary.cactus.email@gmail.com>
2022-09-29 17:27:26 -0700

    add `open-project` autocomplete sketch

--- src/programs_list.rs
+++ src/programs_list.rs
@@ -3,8 +3,11 @@ use iced::{ Alignment, Column, Command, Container, Element, Length, Scrollable, 
 use crate::autocomplete::{
 	file_autocomplete,
 	file_fuzzyfind,
+	project_autocomplete,
+	project_fuzzyfind,
 };
 use crate::launcher::launch_program;
+use crate::path_interpreter::get_projects;
 use crate::style;
 use crate::types;
 
@@ -27,11 +30,14 @@ pub enum State {
 pub struct View {
 	input_state: text_input::State,
 	programs: Option<types::SharedVec<String>>,
+	projects: Option<Vec<String>>,
 	programs_autocomplete: Option<Vec<String>>,
 	programs_fuzzy: Option<Vec<String>>,
+	projects_autocomplete: Option<Vec<String>>,
+	projects_fuzzy: Option<Vec<String>>,
 	scroll_state: scrollable::State,
 	search: String,
-	selected_program: Option<usize>,
+	selected: Option<usize>,
 	state: State,
 }
 
@@ -40,11 +46,14 @@ impl View {
 		View {
 			input_state: text_input::State::focused(),
 			programs,
+			projects: get_projects(),
 			programs_autocomplete: None,
 			programs_fuzzy: None,
+			projects_autocomplete: None,
+			projects_fuzzy: None,
 			scroll_state: scrollable::State::new(),
 			search: String::new(),
-			selected_program: None,
+			selected: None,
 			state: State::ShowFuzzy,
 		}
 	}
@@ -66,65 +75,108 @@ impl View {
 	}
 
 	// detect if we are in project list mode
-	fn is_project_mode(search: &String) -> bool {
-		if &search[0..12] == "open-project" {
-			return true;
+	fn get_project(search: &String) -> Option<String> {
+		if search.len() >= 13 && &search[0..12] == "open-project" {
+			Some(search[13..].to_string())
 		} else {
-			return false;
+			None
+		}
+	}
+
+	fn get_command(search: &String, index: &Option<usize>, list: Option<&Vec<String>>) -> String {
+		if let Some(index) = index {
+			let list = if list.is_none() {
+				return String::from("");
+			} else {
+				list.unwrap()
+			};
+
+			if View::get_project(search).is_some() {
+				format!("open-project {}", list[*index])
+			} else {
+				list[*index].clone()
+			}
+		} else {
+			search.clone()
 		}
 	}
 
 	pub fn update(&mut self, message: Message) -> Command<Message> {
 		match message {
 			Message::Autocomplete => {
-				if let Some(autocomplete) = self.programs_autocomplete.as_ref() {
+				let list = if View::get_project(&self.search).is_some() {
+					self.projects_autocomplete.as_ref()
+				} else {
+					self.programs_autocomplete.as_ref()
+				};
+
+				if let Some(autocomplete) = list {
 					if autocomplete.len() == 0 {
 						return Command::none();
 					}
 
 					self.state = State::ShowAutocomplete;
-					self.selected_program = Some(0);
-					self.search = autocomplete[0].clone();
+					self.selected = Some(0);
 
-					self.programs_autocomplete = file_autocomplete(&self.programs.as_ref().unwrap(), &self.search);
-					self.programs_fuzzy = file_fuzzyfind(&self.programs.as_ref().unwrap(), &self.search);
+					if View::get_project(&self.search).is_some() {
+						self.search = format!("open-project {}", autocomplete[0]);
+					} else {
+						self.search = autocomplete[0].clone();
+					}
+
+					if let Some(project) = View::get_project(&self.search) {
+						self.projects_autocomplete = project_autocomplete(&self.projects.as_ref().unwrap(), &project);
+						self.projects_fuzzy = project_fuzzyfind(&self.projects.as_ref().unwrap(), &project);
+					} else {
+						self.programs_autocomplete = file_autocomplete(&self.programs.as_ref().unwrap(), &self.search);
+						self.programs_fuzzy = file_fuzzyfind(&self.programs.as_ref().unwrap(), &self.search);
+					}
 				}
 			},
 			Message::StartProgram => {
-				if let Some(index) = self.selected_program {
-					let list = View::get_list_from_state(self.state, &self.programs_autocomplete, &self.programs_fuzzy).as_ref();
-					if let Some(list) = list {
-						launch_program(list[index].clone());
-					}
+				let list = if View::get_project(&self.search).is_some() {
+					View::get_list_from_state(self.state, &self.projects_autocomplete, &self.projects_fuzzy).as_ref()
 				} else {
-					launch_program(self.search.clone());
-				}
+					View::get_list_from_state(self.state, &self.programs_autocomplete, &self.programs_fuzzy).as_ref()
+				};
+
+				launch_program(View::get_command(&self.search, &self.selected, list));
 			},
 			Message::SelectUp => {
-				let list = View::get_list_from_state(self.state, &self.programs_autocomplete, &self.programs_fuzzy).as_ref();
+				let list = if View::get_project(&self.search).is_some() {
+					View::get_list_from_state(self.state, &self.projects_autocomplete, &self.projects_fuzzy).as_ref()
+				} else {
+					View::get_list_from_state(self.state, &self.programs_autocomplete, &self.programs_fuzzy).as_ref()
+				};
+
 				if let None = list {
-					self.selected_program = None;
-				} else if let None = self.selected_program {
-					self.selected_program = Some(0);
-				} else if self.selected_program.unwrap() != list.unwrap().len() - 1 {
-					self.selected_program = Some(self.selected_program.unwrap() + 1);
+					self.selected = None;
+				} else if let None = self.selected {
+					self.selected = Some(0);
+				} else if self.selected.unwrap() != list.unwrap().len() - 1 {
+					self.selected = Some(self.selected.unwrap() + 1);
 				}
 			},
 			Message::SelectDown => {
-				if let None = self.selected_program {
-					self.selected_program = Some(0);
-				} else if self.selected_program.unwrap() != 0 {
-					self.selected_program = Some(self.selected_program.unwrap() - 1);
+				if let None = self.selected {
+					self.selected = Some(0);
+				} else if self.selected.unwrap() != 0 {
+					self.selected = Some(self.selected.unwrap() - 1);
 				}
 			},
 			Message::Typed(search) => {
 				self.state = State::ShowFuzzy;
-				self.selected_program = None;
+				self.selected = None;
 				self.search = search;
 				self.input_state.move_cursor_to_end();
 
-				self.programs_autocomplete = file_autocomplete(&self.programs.as_ref().unwrap(), &self.search);
-				self.programs_fuzzy = file_fuzzyfind(&self.programs.as_ref().unwrap(), &self.search);
+				if let Some(project) = View::get_project(&self.search) {
+					self.projects_autocomplete = project_autocomplete(&self.projects.as_ref().unwrap(), &project);
+					self.projects_fuzzy = project_fuzzyfind(&self.projects.as_ref().unwrap(), &project);
+				} else {
+					self.programs_autocomplete = file_autocomplete(&self.programs.as_ref().unwrap(), &self.search);
+					self.programs_fuzzy = file_fuzzyfind(&self.programs.as_ref().unwrap(), &self.search);
+				}
 			},
 		}
 
@@ -133,7 +185,13 @@ impl View {
 
 	pub fn view(&mut self) -> Element<Message> {
 		let mut scrollable = Scrollable::new(&mut self.scroll_state);
-		let list = View::get_list_from_state(self.state, &self.programs_autocomplete, &self.programs_fuzzy).as_ref();
+
+		let list = if View::get_project(&self.search).is_some() {
+			View::get_list_from_state(self.state, &self.projects_autocomplete, &self.projects_fuzzy).as_ref()
+		} else {
+			View::get_list_from_state(self.state, &self.programs_autocomplete, &self.programs_fuzzy).as_ref()
+		};
+
 		if let Some(programs) = list.as_ref() {
 			let mut index = 0;
 			for autocomplete in programs.iter() {
@@ -147,7 +205,7 @@ impl View {
 						.padding(3)
 						.width(Length::Fill)
 						.style(
-							if self.selected_program.is_some() && index == self.selected_program.unwrap() {
+							if self.selected.is_some() && index == self.selected.unwrap() {
 								Box::new(style::SelectedProgram) as Box<dyn container::StyleSheet>
 							} else {
 								Box::new(style::Program) as Box<dyn container::StyleSheet>
